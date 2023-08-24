d33558b89bab02cbc75c5337890e17596e9056db
Mitch Miller <mitch.j.miller@gmail.com>
2023-08-24 21:23:03 +0100

    Added enemy alert sounds

--- Assets/Scripts/Enemy/EnemyAudio.cs
+++ Assets/Scripts/Enemy/EnemyAudio.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class EnemyAudio : MonoBehaviour {
   [SerializeField] private Enemy enemy;
+  [SerializeField][Range(0, 1)] private float footstepVolume = 0.6f;
   [SerializeField] private AudioClip[] footsteps;
+  [SerializeField][Range(0, 1)] private float alertVolume = 1f;
+  [SerializeField] private AudioClip alertSound;
   private AudioSource source;
 
   private void Awake() {
@@ -12,15 +15,25 @@ public class EnemyAudio : MonoBehaviour {
   }
 
   private void Start() {
+    enemy.OnStateChange.AddListener(OnStateChange);
     enemy.OnFootstep.AddListener(OnFootstep);
   }
 
   private void OnDestroy() {
+    enemy.OnStateChange.RemoveListener(OnStateChange);
     enemy.OnFootstep.RemoveListener(OnFootstep);
   }
 
   public void OnFootstep() {
     int index = Random.Range(0, footsteps.Length);
-    source.PlayOneShot(footsteps[index]);
+    source.PlayOneShot(footsteps[index], footstepVolume);
+  }
+
+  private void OnStateChange() {
+    if (enemy.GetState() == "EnemyAlertState") OnAlert();
+  }
+
+  private void OnAlert() {
+    source.PlayOneShot(alertSound, alertVolume);
   }
 }
